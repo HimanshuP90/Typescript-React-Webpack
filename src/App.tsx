de268c5c2a5fb396bdf56a379ef3af24bd934045
Himanshu Pandey <himanshup895@gmail.com>
2021-05-08 23:15:42 +0530

    eslint husky webpack dev added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
-function App() {
-    return (
-        <div>
-            React Typescript
-        </div>
-    )
+import './style.css'
+import linkedIn from './linkedin.svg'
+const App = () => {
+  return (
+    <div>
+      <h1>React Typescript</h1>
+      <p>ESLint Huksy added</p>
+      <img src={linkedIn} alt="linkedin" />
+    </div>
+  )
 }
 
 export default App
